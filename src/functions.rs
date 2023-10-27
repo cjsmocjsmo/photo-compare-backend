9e83fdd31714e844126b4d5930e8a8289c052f25
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-10-27 00:03:03 -0700

    fix addr

--- src/functions.rs
+++ src/functions.rs
@@ -6,8 +6,8 @@ use std::str::FromStr;
 pub fn gen_server_addr() -> SocketAddr {
     let raw_addr = env::var("COMPARE_RAW_HTTP").expect("COMPARE_RAW_HTTP not set");
     let compare_v4_addr = Ipv4Addr::from_str(&raw_addr).unwrap();
-    let port: u16 = env::var("COMPARE_SERVER_PORT")
-        .expect("COMPARE_SERVER_PORT not set")
+    let port: u16 = env::var("COMPARE_PORT")
+        .expect("COMPARE_PORT not set")
         .parse()
         .unwrap();
     let socket = SocketAddr::new(IpAddr::V4(compare_v4_addr), port);
