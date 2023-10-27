2a4e83c65311dd76b3c9e042b2c7642c6ecfefb0
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-10-27 00:33:04 -0700

    added cors

--- src/envvars.rs
+++ src/envvars.rs
@@ -1,19 +1,19 @@
 use std::env;
 
 pub fn set_env_vars() {
-
-
-    // let compare = env::var("COMPARE_PATH");
-    // if compare.is_err() {
-    //     env::set_var("COMPARE_PATH", "/usr/share/compare/compare");
-    // };
     let compare_thumbs = env::var("COMPARE_IMAGE_PATH");
     if compare_thumbs.is_err() {
-        env::set_var("COMPARE_IMAGE_PATH", "/media/pipi/e9535df1-d952-4d78-b5d7-b82e9aa3a975/Converted");
+        env::set_var(
+            "COMPARE_IMAGE_PATH",
+            "/media/pipi/e9535df1-d952-4d78-b5d7-b82e9aa3a975/Converted",
+        );
     };
     let compare_nfo = env::var("COMPARE_JSON_PATH");
     if compare_nfo.is_err() {
-        env::set_var("COMPARE_JSON_PATH", "/media/pipi/e9535df1-d952-4d78-b5d7-b82e9aa3a975/ToRemove");
+        env::set_var(
+            "COMPARE_JSON_PATH",
+            "/media/pipi/e9535df1-d952-4d78-b5d7-b82e9aa3a975/ToRemove",
+        );
     };
     let compare_raw_http = env::var("COMPARE_RAW_HTTP");
     if compare_raw_http.is_err() {
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use actix_cors::Cors;
 use actix_files as fs;
 use actix_web::{App, HttpServer};
 use std::env;
@@ -10,22 +11,20 @@ async fn main() -> std::io::Result<()> {
     let _vars = envvars::set_env_vars();
     let img_path = env::var("COMPARE_IMAGE_PATH").unwrap();
     let json_path = env::var("COMPARE_JSON_PATH").unwrap();
-
     let socket = functions::gen_server_addr();
 
     HttpServer::new(move || {
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allow_any_method()
+            .allow_any_header()
+            .max_age(3600);
         App::new()
+            .wrap(cors)
             .service(functions::test)
-            // .service(crate::functions::echo)
-
-
-            // .service(crate::server::server_functions::voyager)
-            // .service(crate::server::server_functions::wheeloftime)
             .service(fs::Files::new("/json", json_path.clone()).show_files_listing())
             .service(fs::Files::new("/image", img_path.clone()).show_files_listing())
-        }
-    )
-    // .bind(("192.168.0.26", 8080))?
+    })
     .bind(socket)?
     .run()
     .await
