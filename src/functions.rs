7a263589cc43d058a5cec7c24fcfacf3714186a8
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-10-29 14:23:28 -0700

    update

--- src/functions.rs
+++ src/functions.rs
@@ -73,7 +73,7 @@ pub async fn jsonblob() -> impl Responder {
 //     "fuck".to_string()
 // }
 
-fn get_25_files() -> Vec<TransDupsEntry> {
+fn get_25_files() -> Vec<String> {
     let json_path = env::var("COMPARE_JSON_PATH").unwrap();
     let pagination = env::var("COMPARE_PAGINATION").unwrap();
 let int_pagination =  pagination.parse::<usize>().unwrap();
@@ -84,9 +84,9 @@ let int_pagination =  pagination.parse::<usize>().unwrap();
         if entry.file_type().is_file() {
             let file_path = entry.path().to_str().unwrap().to_owned();
             // read the json file file_path and parse it into a ImgHashStruct
-            let file = std::fs::read_to_string(file_path).unwrap();
-            let img_hash_struct = serde_json::from_str(&file).unwrap();
-            files.push(img_hash_struct);
+            let file_contents = std::fs::read_to_string(file_path).unwrap();
+            // let img_hash_struct = serde_json::from_str(&file).unwrap();
+            files.push(file_contents);
 
             if files.len() == int_pagination {
                 break;
