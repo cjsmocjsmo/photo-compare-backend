9d08a168a321e338fdcab53dfa7c11d2f4c18228
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-10-29 10:43:14 -0700

    update

--- src/envvars.rs
+++ src/envvars.rs
@@ -1,15 +1,19 @@
 use std::env;
 
 pub fn set_env_vars() {
-    let compare_thumbs = env::var("COMPARE_IMAGE_PATH");
-    if compare_thumbs.is_err() {
+    let compare_pagination = env::var("COMPARE_PAGINATION");
+    if compare_pagination.is_err() {
+        env::set_var("COMPARE_PAGINATION", "25");
+    };
+    let compare_image_path = env::var("COMPARE_IMAGE_PATH");
+    if compare_image_path.is_err() {
         env::set_var(
             "COMPARE_IMAGE_PATH",
             "/media/pipi/e9535df1-d952-4d78-b5d7-b82e9aa3a975/Converted",
         );
     };
-    let compare_nfo = env::var("COMPARE_JSON_PATH");
-    if compare_nfo.is_err() {
+    let compare_json_path = env::var("COMPARE_JSON_PATH");
+    if compare_json_path.is_err() {
         env::set_var(
             "COMPARE_JSON_PATH",
             "/media/pipi/e9535df1-d952-4d78-b5d7-b82e9aa3a975/ToRemove",
--- src/functions.rs
+++ src/functions.rs
@@ -33,6 +33,49 @@ pub struct TransDupsEntry {
 #[get("/jsonblob")]
 pub async fn jsonblob() -> impl Responder {
     let json_path = env::var("COMPARE_JSON_PATH").unwrap();
+    let pagination = env::var("COMPARE_PAGINATION").unwrap();
+
+    let dup_info = get_25_files();
+    println!("dup_info {:#?}", dup_info.clone());
+
+    // let gen_frag = generate_fragment(dup_info);
+
+
+    let json = serde_json::to_string(&dup_info).unwrap();
+
+    // println!("Found {} files", json.len());
+
+    HttpResponse::Ok().json(json)
+}
+
+// fn generate_fragment(dupslist: Vec<TransDupsEntry>) -> String {
+
+//     for file in dupslist {
+//         let mut fragment = Vec::new();
+//         let filename = file.clone().httpfilename;
+//         let frag1 = format!("<div class='container'><h1>Original</h1>");
+//         fragment.push(frag1);
+//         let frag2 = format!("<section class='containerImg'>");
+//         fragment.push(frag2);
+//         let frag3 = format!("<img src={} alt='test1'></section>", file.clone().httpfilename);
+//         fragment.push(frag3);
+//         let frag4 = format!("<h1>Duplicates</h1><section class='dupImages'>");
+//         fragment.push(frag4);
+//         for dup in file.duplicates {
+//             let frag5 = format!("<div class='dupCard'><img src={} alt='test2'>", dup.clone().httpdups);
+//             fragment.push(frag5);
+//             let frag6 = format!("<div class='dupCardText'><p>{}</p></div></div>", dup.clone().strdups);
+//             fragment.push(frag6);
+//         }
+
+//     }
+
+//     "fuck".to_string()
+// }
+
+fn get_25_files() -> Vec<TransDupsEntry> {
+    let json_path = env::var("COMPARE_JSON_PATH").unwrap();
+    let pagination = env::var("COMPARE_PAGINATION").unwrap();
 
     let mut files = Vec::new();
 
@@ -44,15 +87,12 @@ pub async fn jsonblob() -> impl Responder {
             let file = std::fs::read_to_string(file_path).unwrap();
             let img_hash_struct: TransDupsEntry = serde_json::from_str(&file).unwrap();
             files.push(img_hash_struct);
-            if files.len() == 25 {
+            let int_pagination =  pagination.parse::<usize>().unwrap();
+            if files.len() == int_pagination {
                 break;
             }
         }
     }
 
-    let json = serde_json::to_string(&files).unwrap();
-
-    println!("Found {} files", files.len());
-
-    HttpResponse::Ok().json(json)
+    files
 }
